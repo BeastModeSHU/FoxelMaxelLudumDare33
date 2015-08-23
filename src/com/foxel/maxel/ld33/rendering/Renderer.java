6f0bb3c5cd8cae79f0965ed06ce7bbf900502e3d
M. Walker <masimba@blueyonder.co.uk>
2015-08-23 19:23:40 +0100

    Deleting unused methods

--- src/com/foxel/maxel/ld33/rendering/Renderer.java
+++ src/com/foxel/maxel/ld33/rendering/Renderer.java
@@ -33,48 +33,6 @@ public class Renderer {
 		this.CEILING_LAYER = Constants.CEILING_LAYER_ID;
 	}
 
-	// TODO rename to getBelowEntities();
-	public int[] getBelowPlayer() {
-		/*
-		 * float playerY = player.getPixelLocation().y +
-		 * player.getEntityDimensions().y;
-		 * 
-		 * boolean stopLoop = false; int counter = 0;
-		 * 
-		 * while (!stopLoop && counter < map.getHeightInTiles()) { float tileY =
-		 * (counter * Constants.TILESIZE) + Constants.TILESIZE;
-		 * 
-		 * if (playerY < tileY) { stopLoop = true; } ++counter; }
-		 * 
-		 * int x = 0; int y = 0; int startX = x; int startY = y; int width = 15;
-		 * int height = 0 + (counter);
-		 * 
-		 * return new int[] { x, y, startX, startY, width, height };
-		 */
-		return null;
-	}
-
-	// TODO rename to getAboveEntities();
-	public int[] getAbovePlayer() {
-
-		float playerY = player.getPixelLocation().y + player.getEntityDimensions().y;
-		boolean stopLoop = false;
-		int counter = 0;
-
-		while (!stopLoop && counter < map.getHeightInTiles()) {
-			float tileY = (counter * Constants.TILESIZE) + Constants.TILESIZE;
-			if (playerY < tileY) {
-				stopLoop = true;
-			}
-			++counter;
-		}
-		int x = 0;
-		int y = (counter - 1);
-		int width = 15;
-		int height = 16 - (counter);
-
-		return new int[] { x, y * Constants.TILESIZE, x, y, width, height };
-	}
 
 	public void sortRenderableByZ() {
 		int j = 0;
